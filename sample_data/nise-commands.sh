2941eee0143d8351d9180a9b40ce8d8b0be19382
Cloud User <ec2-user@clientvm.7371.internal>
2019-09-06 20:14:23 +0000

    insights upload OK

--- sample_data/nise-commands.sh
+++ sample_data/nise-commands.sh
@@ -3,3 +3,9 @@ nise --ocp --ocp-cluster-id gpte-sample-cluster --static-report-file ocp_gpte_sa
 
 nise --aws --static-report-file .//aws_gpte_sample_ILT.yaml --aws-s3-bucket-name cost-test-bucket  --aws-s3-report-name cur
 INSIGHTS_USER=insights-upload-rhte19mbu1org INSIGHTS_PASSWORD=yaR3dBRKg2iPkPy nise --ocp --ocp-cluster-id cost-test-1 --static-report-file ocp_gpte_sample_ILT.yaml --insights-upload https://cloud.redhat.com/api/ingress/v1/upload
+
+
+INSIGHTS_USER=insights-upload-rhte19mbu2org INSIGHTS_PASSWORD=yaR3dBRKg2iPkPy nise --ocp --ocp-cluster-id ocp-cluster-example-1 --static-report-file ocp_gpte_sample_ILT.yaml --insights-upload https://cloud.redhat.com/api/ingress/v1/upload
+
+
+
